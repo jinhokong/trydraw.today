396df1e086e10a6e9fb10c29447fb5abee3f192e
jinho hong <tpdleps@gmail.com>
2019-05-01 23:43:27 +0900

    CI test

--- packages/infra/deploy.sh
+++ packages/infra/deploy.sh
@@ -1,3 +1,6 @@
+rm -rf .terraform
+rm -rf terraform.tfstate.d
+aws configure get aws_access_key_id --profile trydrawtoday
 terraform init
 terraform workspace new staging
 terraform workspace select staging
