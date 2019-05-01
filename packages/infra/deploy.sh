0e3a057c12d5954a3a13122556d37ac8d9a8e053
jinho hong <tpdleps@gmail.com>
2019-05-02 00:59:55 +0900

    test

--- packages/infra/deploy.sh
+++ packages/infra/deploy.sh
@@ -1,6 +1,6 @@
 rm -rf .terraform
 rm -rf terraform.tfstate.d
-TF_LOG = DEBUG terraform init
+terraform init
 terraform workspace new staging
 terraform workspace select staging
 terraform apply -input=false
